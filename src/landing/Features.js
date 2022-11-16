5f944d2a46624d20e3d044a45e709be6b1d0501d
rashed <x.suwaidi@gmail.com>
2022-11-16 09:52:03 +0400

    change way looks

--- src/landing/Features.js
+++ src/landing/Features.js
@@ -44,12 +44,11 @@ export default function Features() {
         <div className="mt-20 max-w-lg sm:mx-auto md:max-w-none">
           <div className="grid grid-cols-1 gap-y-16 md:grid-cols-2 md:gap-x-12 md:gap-y-16">
             {features.map((feature) => (
-              <div key={feature.name} className="relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row">
-                <div className="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-500 text-white sm:shrink-0">
-                  <feature.icon className="h-8 w-8" aria-hidden="true" />
-                </div>
+              <div key={feature.name} className="relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row ">
+          
+          
                 <div className="sm:min-w-0 sm:flex-1">
-                  <p className="text-lg font-semibold leading-8 text-gray-900 text-end">{feature.name}</p>
+                  <p className="text-lg font-bold leading-8 text-gray-900 text-end">{feature.name} </p>
                 </div>
               </div>
             ))}
